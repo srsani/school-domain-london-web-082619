c530c9ad261bda2d50097686c4c6c2cde1ae3c7f
droll-accelerator-1483 <srsani@gmail.com>
2019-08-28 15:43:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  def initialize (name, roster = {})
+    @name = name
+    @roster = roster
+  end
+  def name
+    @name
+  end
+  def add_student(student_name,grade)
+    if @roster.key?(grade)
+      @roster[grade] = @roster[grade].append(student_name)
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+  def roster
+    @roster
+  end
+  def grade(grade)
+    @roster[grade]
+  end 
+  def sort
+    for i,j in @roster
+      @roster[i] = j.sort
+    end
+    @roster
+  end
+  
+end 
