d27c6fd09b8deeee067782df0e65af4f26d4e5df
droll-accelerator-1483 <srsani@gmail.com>
2019-08-28 15:37:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,19 @@
 class School
-  def initialize (name)
+  def initialize (name, roster = {})
     @name = name
+    @roster = roster
+  end
+  def name
+    @name
+  end
+  def add_student(student_name,grade)
+    if @roster.key?(grade)
+      @roster[grade] = @roster[grade].append(student_name)
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+  def roster
+    @roster
   end
 end 
